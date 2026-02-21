dd86207e4d0617c9baeb3c091c0f0e0e52f256fd
SamHacker <xux510208@gmail.com>
2026-02-21 10:45:49 +0800

    ✨ feat(background): 添加背景圖片以增強視覺效果

--- src/components/BackgroundImage.vue
+++ src/components/BackgroundImage.vue
@@ -1,7 +1,20 @@
 <template>
-  <div class="absolute"></div>
+  <div class="absolute top-0 left-0 -z-2 h-[300px] w-screen opacity-40">
+    <img
+      :src="bgImage"
+      alt="Cover Image"
+      class="gradient-mask h-full w-full bg-top object-cover"
+    />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import bgImage from "@/assets/background/background_cover.png";
+</script>
 
-<style scoped></style>
+<style scoped>
+.gradient-mask {
+  mask-image: linear-gradient(180deg, #000 0%, rgba(0, 0, 0, 0) 100%);
+  -webkit-mask-image: linear-gradient(180deg, #000 0%, rgba(0, 0, 0, 0) 100%);
+}
+</style>

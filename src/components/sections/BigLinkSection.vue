121063db2ef0a51f8aabcc6c4ae2ebefd98af0f0
SamHacker <xux510208@gmail.com>
2026-02-21 18:08:57 +0800

    💄 style(BigLinkSection): 調整大連結區域的佈局樣式以改善顯示效果

--- src/components/sections/BigLinkSection.vue
+++ src/components/sections/BigLinkSection.vue
@@ -1,6 +1,8 @@
 <template>
   <!-- 大連結區域 -->
-  <div class="grid w-full grid-cols-2 gap-4">
+  <div
+    class="grid w-full grid-cols-1 grid-rows-2 gap-4 sm:grid-cols-2 sm:grid-rows-1"
+  >
     <FeaturedLink
       :href="link.href"
       :icon="link.icon"

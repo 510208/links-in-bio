9f05d94089e2134a62faed2b619946c5ffcac425
SamHacker <xux510208@gmail.com>
2026-02-21 12:21:30 +0800

    ✨ feat(BigLinkSection, ListLinkSection): 新增大連結區域和條列連結區域組件並重構連結區段

---
+++ src/components/sections/BigLinkSection.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- 大連結區域 -->
+  <div class="grid w-full max-w-[1000px] min-w-[400px] grid-cols-2 gap-2.5">
+    <FeaturedLink
+      :href="link.href"
+      :icon="link.icon"
+      :label="link.label"
+      :title="link.title"
+      :background-image="link.bgImage"
+      v-for="link in config.links.big"
+    />
+  </div>
+</template>
+
+<script setup>
+import FeaturedLink from "./FeaturedLink.vue";
+
+import { Globe, Newspaper } from "lucide-vue-next";
+
+import config from "@shConfig";
+</script>
---
+++ src/components/sections/ListLinkSection.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- 條列連結區域 -->
+  <div class="flex w-full flex-col gap-4">
+    <!-- 第一行：兩個Discord按鈕 -->
+    <div class="flex flex-col justify-between gap-4 md:flex-row">
+      <LinkButton
+        href="https://discord.gg/samhacker"
+        :icon="DiscordIcon"
+        label="Discord (Main)"
+        icon-class="text-[#5865F2]"
+      />
+      <LinkButton
+        href="https://discord.gg/samhacker-alt"
+        :icon="DiscordIcon"
+        label="Discord (Alt)"
+        icon-class="text-[#5865F2]"
+      />
+    </div>
+
+    <!-- 其他單行按鈕 -->
+    <LinkButton
+      :href="link.href"
+      :icon="link.icon"
+      :label="link.label"
+      icon-class="white"
+      v-for="link in config.links.list"
+    />
+  </div>
+</template>
+
+<script setup>
+import LinkButton from "./LinkButton.vue";
+import config from "@shConfig";
+
+import { DiscordIcon } from "vue3-simple-icons";
+</script>

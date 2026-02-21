f7feccb7b86872bb2d0f9e67bc74c5653a581434
SamHacker <xux510208@gmail.com>
2026-02-21 12:20:06 +0800

    ✨ feat(config): 新增 shLinks 配置檔案，包含連結和圖示設定

---
+++ shlinks.config.ts
@@ -0,0 +1,87 @@
+import { Globe, Newspaper } from "lucide-vue-next";
+
+import {
+  GitHubIcon,
+  ThreadsIcon,
+  InstagramIcon,
+  YouTubeIcon,
+  WakaTimeIcon,
+  XIcon,
+  FigmaIcon,
+  SteamIcon,
+  RedditIcon,
+} from "vue3-simple-icons";
+
+import designImage1 from "@/assets/links/mega-link_bg_1.png";
+import designImage2 from "@/assets/links/mega-link_bg_2.png";
+import type { Config } from "@/types/config.d";
+
+const config: Config = {
+  links: {
+    big: [
+      {
+        label: "個人網站",
+        href: "https://510208.github.io",
+        title: "Introduction",
+        icon: Globe,
+        bgImage: designImage1,
+      },
+      {
+        label: "部落格",
+        href: "https://samhacker.xyz",
+        title: "Blog",
+        icon: Newspaper,
+        bgImage: designImage2,
+      },
+    ],
+    list: [
+      {
+        label: "GitHub",
+        icon: GitHubIcon,
+        href: "https://github.com/510208",
+      },
+      {
+        label: "Threads",
+        icon: ThreadsIcon,
+        href: "https://www.threads.com/@samhacker.xyz",
+      },
+      {
+        label: "Instagram",
+        icon: InstagramIcon,
+        href: "https://www.instagram.com/samhacker.xyz",
+      },
+      {
+        label: "YouTube",
+        icon: YouTubeIcon,
+        href: "https://www.youtube.com/channel/UC6orwHdQNVzwHsA6M7HYD9g",
+      },
+      {
+        label: "WakaTime",
+        icon: WakaTimeIcon,
+        href: "https://wakatime.com/@SamHacker",
+      },
+      {
+        label: "Twitter",
+        icon: XIcon,
+        href: "https://x.com/xux510208",
+      },
+      {
+        label: "Figma",
+        icon: FigmaIcon,
+        href: "https://www.figma.com/@samhacker",
+      },
+      {
+        label: "Steam",
+        icon: SteamIcon,
+        href: "https://steamcommunity.com/id/PB510208/",
+      },
+      {
+        label: "Reddit",
+        icon: RedditIcon,
+        href: "https://www.reddit.com/user/DiligentCarpenter421/",
+      },
+    ],
+  },
+};
+
+export default config;
---
+++ src/types/config.d.ts
@@ -0,0 +1,18 @@
+export interface Config {
+  links: {
+    list: {
+      big: {
+        label: string;
+        href: string;
+        title: string;
+        icon: any;
+        bgImage: string;
+      }[];
+      list: {
+        label: string;
+        icon: any;
+        href: string;
+      }[];
+    };
+  };
+}

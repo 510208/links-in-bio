e7b5fe7fb9307380183ec94afd28766326b3616f
SamHacker <xux510208@gmail.com>
2026-02-21 11:45:47 +0800

    ✨ feat(Introduction): 更新個人介紹，新增學生身份與地點資訊

--- src/components/sections/Introduction.vue
+++ src/components/sections/Introduction.vue
@@ -1,10 +1,23 @@
 <template>
   <div class="flex flex-col items-center gap-2.5 pt-50 pb-17.5">
+    <Avatar class="h-25 w-25 shadow-lg">
+      <AvatarImage
+        src="https://gravatar.com/avatar/f7598bd8d4aba38d7219341f81a162fc842376b3b556b1995cbb97271d9e2915?size=256"
+      />
+      <AvatarFallback>SH</AvatarFallback>
+    </Avatar>
     <h1 class="text-center text-4xl font-bold text-front-100">SamHacker</h1>
-    <p class="text-center text-lg text-gray-500">
-      Software Engineer | Tech Enthusiast
-    </p>
+    <div class="flex items-center gap-2.5">
+      <p class="text-center text-lg text-stone-400">Student</p>
+      <SeparatorDot />
+      <p class="text-center text-lg text-stone-400">@510208</p>
+      <SeparatorDot />
+      <p class="text-center text-lg text-stone-400">Taichung, Taiwan</p>
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import SeparatorDot from "@/components/ui/separator/SeparatorDot.vue";
+</script>

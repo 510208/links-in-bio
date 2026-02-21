cadee448c8d77751f211c4dea529aea8ad190afc
SamHacker <xux510208@gmail.com>
2026-02-21 10:46:02 +0800

    ✨ feat(App): 新增 AppContent 和 BackgroundImage 組件以顯示內容區域

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppContent from "@/components/AppContent.vue";
+import BackgroundImage from "@/components/BackgroundImage.vue";
+</script>
 
-<template></template>
+<template>
+  <div class="flex flex-col items-center">
+    <BackgroundImage />
+
+    <AppContent><h1>SamHacker</h1> </AppContent>
+  </div>
+</template>
 
 <style scoped></style>

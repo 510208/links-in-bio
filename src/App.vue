5c7316303b05c9fd7bd2ec7c7f1777d41a00a099
SamHacker <xux510208@gmail.com>
2026-02-21 10:58:15 +0800

    ✨ feat(App): 新增 Introduction 組件以顯示應用介紹

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
 import AppContent from "@/components/AppContent.vue";
 import BackgroundImage from "@/components/BackgroundImage.vue";
+import Introduction from "@/components/sections/Introduction.vue";
 </script>
 
 <template>
   <div class="flex flex-col items-center">
     <BackgroundImage />
 
-    <AppContent><h1>SamHacker</h1> </AppContent>
+    <AppContent>
+      <Introduction />
+    </AppContent>
   </div>
 </template>
 
---
+++ src/components/sections/Introduction.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-col items-center gap-2.5 pt-50 pb-17.5">
+    <h1 class="text-center text-4xl font-bold text-front-100">SamHacker</h1>
+    <p class="text-center text-lg text-gray-500">
+      Software Engineer | Tech Enthusiast
+    </p>
+  </div>
+</template>
+
+<script setup></script>

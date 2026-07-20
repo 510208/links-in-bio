adedfd0e22f382d5f099652ae51fba424eae14ec
SamHacker <xux510208@gmail.com>
2026-07-20 12:00:33 +0800

    🧑‍💻 feat(vite): 增加 Vue 開發工具插件以改善開發體驗

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,19 @@ import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { fileURLToPath, URL } from "node:url";
 import { imagetools } from "vite-imagetools";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss(), tsconfigPaths(), imagetools()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    tsconfigPaths(),
+    imagetools(),
+    vueDevTools({
+      componentInspector: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
